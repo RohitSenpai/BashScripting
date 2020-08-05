4b65c8bc0d1853cb19444f1ed7ae9a00972a415d
RohitSenpai <rohitpradhan2510@gmail.com>
2020-08-05 23:17:10 +0530

    This file contains the information I learnt today, i.e user inputs and comments.

---
+++ user_inputs_and_comments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#user input. This is just a comment!
+
+read -p "Enter your full name and age: " 
+
+read FNAME LNAME AGE
+
+echo "Your name is $FNAME $LNAME and you're $AGE years old!"
+
