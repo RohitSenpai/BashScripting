ff4563e4356da392116557b9b3c1602c149e6ed5
RohitSenpai <rohitpradhan2510@gmail.com>
2020-08-10 22:41:20 +0530

    Added comments.

--- loops.sh
+++ loops.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for NAMES in $(cat names.txt); do
+for NAMES in $(cat names.txt); do  #the names.txt file is local to my PC and I'm not uploading it to github.
 	echo "The names are: $NAMES"
 done
 
