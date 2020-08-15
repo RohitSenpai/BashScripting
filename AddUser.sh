147a87088c71a5a71914b07abc45a758c90fbee9
RohitSenpai <rohitpradhan2510@gmail.com>
2020-08-15 23:07:36 +0530

    New comment added in the script telling what the script does.

--- AddUser.sh
+++ AddUser.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#A simple script to add a new user using the concept of positional parameters.
+
 echo "Execution of Script: $0"
 echo "Please enter the name of the user: $1"
 #adding user
